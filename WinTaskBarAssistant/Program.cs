668f88045551035d3b2de44f8a57950a6d8b6433
megadox <dhappymean@gmail.com>
2022-02-18 11:33:23 +0900

    Initialize

---
+++ WinTaskBarAssistant/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinTaskBarAssistant
+{
+    static class Program
+    {
+        /// <summary>
+        /// 해당 응용 프로그램의 주 진입점입니다.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new TaskBar());
+        }
+
+        private static System.Reflection.Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            // 어셈블리가 .exe 파일과 같은 폴더에 있으면 이 메서드는 호출되지 않는다.
+            Assembly thAss = Assembly.GetExecutingAssembly();
+
+            // Get Assembly Name
+            var AssName = args.Name.Substring(0, args.Name.IndexOf(',')) + ".dll";
+            //MessageBox.Show("AssName : " + AssName.ToString());
+
+            var resources = thAss.GetManifestResourceNames().Where(s => s.EndsWith(AssName));
+            if (resources.Count() > 0)
+            {
+                var resourceName = resources.First();
+
+                //MessageBox.Show("Resource Name : " + resourceName.ToString());
+
+                using (Stream stream = thAss.GetManifestResourceStream(resourceName))
+                {
+                    if (stream == null)
+                    {
+                        return null;
+                    }
+
+                    var block = new byte[stream.Length];
+                    stream.Read(block, 0, block.Length);
+                    return Assembly.Load(block);
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ WinTaskBarAssistant/TaskBar.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WinTaskBarAssistant
+{
+    public partial class TaskBar : Form
+    {
+        Timer ttimer;
+
+        public TaskBar()
+        {
+            InitializeComponent();
+
+            
+            this.Size = new Size(1, 1);
+            this.TopMost = false;
+            //this.TopLevel = false;
+
+            this.Load += Form1_Load;
+
+            taskbarAssistant1.ProgressMaximumValue = 100;
+            taskbarAssistant1.ProgressMode = DevExpress.Utils.Taskbar.Core.TaskbarButtonProgressMode.Paused;
+
+            ttimer = new Timer();
+            ttimer.Interval = 40;
+            ttimer.Tick += T_Tick;
+            ttimer.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Opacity = 0;
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void T_Tick(object sender, EventArgs e)
+        {
+            if (taskbarAssistant1.ProgressCurrentValue < 100)
+                taskbarAssistant1.ProgressCurrentValue++;
+            else
+                taskbarAssistant1.ProgressCurrentValue = 0;
+        }
+
+        public void Stop()
+        {
+            ttimer.Stop();
+            this.Close();            
+            //Application.Exit();
+        }
+    }
+}
